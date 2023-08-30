2933e4568f31dc7b315f7cad85f0ddd8ac7dc403
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-08-30 20:23:29 +0200

    add

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 app.use(loggingMiddleware);
-app.use(cors())
+app.use(cors()) 
 
 app.use("/api/github", gitRouter)
 app.use("/api/contact", limiterMiddleware, contactRouter)
